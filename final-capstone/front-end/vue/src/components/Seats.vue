dbb710909a9c4da7f863c001f37a682449d36416
Boris Isakov <bisakov16@gmail.com>
2020-12-14 10:31:28 -0500

    Moved form from SeatsPage to Seats component

--- final-capstone/front-end/vue/src/components/Seats.vue
+++ final-capstone/front-end/vue/src/components/Seats.vue
@@ -29,14 +29,26 @@
                     <button v-on:click="decrementSeniorCount(), totalTicketCount()">-</button>
                 </div>
         </div>
-     <div class="total-tickets">
-        <h2>Total Tickets</h2>
-        <h3>{{totalTickets}}</h3>
-     </div>
+                <div class="total-tickets">
+                <h2>Total Tickets</h2>
+                <h3>{{totalTickets}}</h3>
+                </div>
     
 
-        </div>
-    </div>
+            </div>
+            <div class="form-container">
+            <form class="checkout-form">
+            <label for="fname">First name:</label><br>
+            <input type="text" id="fname" name="fname"><br>
+            <label for="lname">Last name:</label><br>
+            <input type="text" id="lname" name="lname">
+            <label for="email">Email:</label><br>
+            <input type="text" id="email" name="email">
+            <label for="billing">Billing Address:</label><br>
+            <input type="text" id="billing" name="billing">
+            </form>
+            </div>
+       </div>
 
 
     
--- final-capstone/front-end/vue/src/views/SeatsPage.vue
+++ final-capstone/front-end/vue/src/views/SeatsPage.vue
@@ -9,18 +9,7 @@
         <tickets />
       </div>-->
     </div>
-    <div>
-      <form class="checkout-form">
-        <label for="fname">First name:</label><br>
-        <input type="text" id="fname" name="fname"><br>
-        <label for="lname">Last name:</label><br>
-        <input type="text" id="lname" name="lname">
-        <label for="email">Email:</label><br>
-        <input type="text" id="email" name="email">
-        <label for="billing">Billing Address:</label><br>
-        <input type="text" id="billing" name="billing">
-      </form>
-    </div>
+   
   </div>
 </template>
 
